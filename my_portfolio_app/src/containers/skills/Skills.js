39ac38be70b334731af65e24a1ba8a46f9feba1e
saponalisaju <saponalibpl12486@gmail.com>
2025-10-02 16:37:08 +0600

    oo

--- my_portfolio_app/src/containers/skills/Skills.js
+++ my_portfolio_app/src/containers/skills/Skills.js
@@ -23,7 +23,7 @@ export default function Skills() {
               <img
                 className="man_working"
                 alt="Man Working"
-                src={require("../../assets/images/man_work.WEBP")}
+                src={require("../../assets/images/man_work.webp")}
               ></img>
             )}
           </div>
