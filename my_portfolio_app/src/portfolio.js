44b004dc70cd61f6d05588d0e99cfe8d9cb6f105
saponalisaju <saponalibpl12486@gmail.com>
2025-10-02 10:05:46 +0600

    firstcommit

---
+++ my_portfolio_app/src/portfolio.js
@@ -0,0 +1,433 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation";
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  // animated: true
+};
+
+const greeting = {
+  username: "Md. Sapon Ali",
+  title: "Hi all, I'm Ali",
+  subTitle: emoji(
+    "🚀 Full Stack Web & Android App Developer specializing in the MERN stack. Skilled in building scalable applications using Python/Django, React.js, Node.js, React Native, Java, and other modern technologies."
+  ),
+
+  resumeLink:
+    "https://docs.google.com/document/d/1iv7ts4ILzi6KF1Z5q1j82Gmg7Te-97Z80nk5fCDz2CU/edit?tab=t.0",
+  displayGreeting: true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/saponalisaju",
+  linkedin: "https://www.linkedin.com/in/saponali/",
+  gmail: "mdsaponali124@gmail.com",
+  //gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/?ref=homescreenpwa",
+  //medium: "https://medium.com/@saadpasta",
+  stackoverflow: "https://stackoverflow.com/users/28937055/md-sapon-ali",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "bootstrap",
+      fontAwesomeClassname: "fab fa-bootstrap"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "github",
+      fontAwesomeClassname: "fab fa-github"
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
+    }
+  ],
+  display: true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true,
+  schools: [
+    {
+      schoolName: "National University Bangladesh",
+      logo: require("./assets/images/OIP (1).webp"),
+      subHeader: "Bachelor of Science in Chemistry",
+      duration: "September 2006 - April 2010"
+      //desc: "Participated in the research of XXX and published 3 papers.",
+      // descBullets: [
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      // ]
+    }
+    // {
+    //   schoolName: "Stanford University",
+    //   logo: require("./assets/images/nationalLogo.png"),
+    //   subHeader: "Bachelor of Science in Chemistry",
+    //   duration: "September 2006 - April 2010",
+    //   // desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+    //   descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    // }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "Database",
+      progressPercentage: "78%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Frontend/UI/UX Design",
+      company: "Frontend",
+      companylogo: require("./assets/images/image_f.jpg"),
+      date: "March 2020 ",
+      desc: "Frontend development creates responsive, user-friendly interfaces using HTML, CSS, and JavaScript to deliver seamless, accessible experiences across devices.",
+      descBullets: [
+        "Crafting responsive, user-friendly interfaces with HTML, CSS, JavaScript.",
+        "Transforming designs into interactive, accessible web and mobile experiences."
+      ]
+    },
+    {
+      role: "Backend Development",
+      company: "Backend",
+      companylogo: require("./assets/images/backend_i.png"),
+      date: "May 2021 – Dec 2021",
+      desc: "Backend development handles server logic, database management, and API integration to power secure, scalable applications. It ensures smooth data flow, authentication, and performance behind the scenes of web and mobile platforms."
+    },
+    {
+      role: "Database Management",
+      company: "Database",
+      companylogo: require("./assets/images/database_p.png"),
+      date: "Jan 2022 – Till Now",
+      desc: "A database stores, organizes, and retrieves structured data efficiently. It powers backend systems, enabling secure access, scalability, and performance for applications through queries, indexing, relationships, and real-time data management."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+ To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/Logo 2 Final.png"),
+      projectName: "Travel Test App",
+      projectDesc: "",
+      footerLink: [
+        {
+          name: "Live Demo",
+          url: "https://traveltestdemoapp.netlify.app/"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/logo final.png"),
+      projectName: "E-Commerce App",
+      projectDesc: "",
+      footerLink: [
+        {
+          name: "Live Demo",
+          url: "https://ecomclothes.netlify.app/"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/logo_abc.png"),
+      projectName: "E-commerce Admin",
+      projectDesc: "",
+      footerLink: [
+        {
+          name: "Live Demo",
+          url: "https://ecomclothesadmin.netlify.app/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  // title: emoji("Achievements And Certifications 🏆 "),
+  // subtitle:
+  //   "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+  // achievementsCards: [
+  //   {
+  //     title: "Google Code-In Finalist",
+  //     subtitle:
+  //       "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+  //     image: require("./assets/images/codeInLogo.webp"),
+  //     imageAlt: "Google Code-In Logo",
+  //     footerLink: [
+  //       {
+  //         name: "Certification",
+  //         url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+  //       },
+  //       {
+  //         name: "Award Letter",
+  //         url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+  //       },
+  //       {
+  //         name: "Google Code-in Blog",
+  //         url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     title: "Google Assistant Action",
+  //     subtitle:
+  //       "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+  //     image: require("./assets/images/googleAssistantLogo.webp"),
+  //     imageAlt: "Google Assistant Action Logo",
+  //     footerLink: [
+  //       {
+  //         name: "View Google Assistant Action",
+  //         url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     title: "PWA Web App Developer",
+  //     subtitle: "Completed Certification from SMIT for PWA Web App Development",
+  //     image: require("./assets/images/pwaLogo.webp"),
+  //     imageAlt: "PWA Logo",
+  //     footerLink: [
+  //       {name: "Certification", url: ""},
+  //       {
+  //         name: "Final Project",
+  //         url: "https://pakistan-olx-1.firebaseapp.com/"
+  //       }
+  //     ]
+  //   }
+  // ],
+  // display: true
+};
+
+// Blogs Section
+
+const blogSection = {
+  // title: "Blogs",
+  // subtitle:
+  //   "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  // displayMediumBlogs: "true",
+  // blogs: [
+  //   {
+  //     url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+  //     title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+  //     description:
+  //       "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+  //   },
+  //   {
+  //     url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+  //     title: "Why REACT is The Best?",
+  //     description:
+  //       "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+  //   },
+  //   {
+  //     url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+  //     title: "Why REACT is The Best?",
+  //     description:
+  //       "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+  //   }
+  // ],
+  // display: true
+};
+
+// Talks Sections
+
+const talkSection = {
+  // title: "TALKS",
+  // subtitle: emoji(
+  //   "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  // ),
+  // talks: [
+  //   {
+  //     title: "Build Actions For Google Assistant",
+  //     subtitle: "Codelab at GDG DevFest Karachi 2019",
+  //     slides_url: "https://bit.ly/saadpasta-slides",
+  //     event_url: "https://www.facebook.com/events/2339906106275053/"
+  //   }
+  // ],
+  // display: true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  // title: emoji("Podcast 🎙️"),
+  // subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+  // podcast: [
+  //   "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  // ],
+  // display: true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  display: true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+880-181-851-8891",
+  email_address: "mdsaponali124@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
+};
